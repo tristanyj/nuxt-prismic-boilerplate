3da3124ee12e572871d2e0a71ef55bc59c4aa9cc
Tristan Lanoye <tristanyjlanoye@gmail.com>
2020-04-30 16:41:53 +0200

    fix(config): add .DS_STORE to .gitignore
    skip
    
    skip

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-	extends: ['@commitlint/config-conventional']
+	extends: ["@commitlint/config-conventional"],
+	parserPreset: "conventional-changelog-conventionalcommits",
+	"type-enum": ["build", "ci", "chore", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
+	"scope-enum": [2, "always", ["core", "config", "component", "layout", "asset", "env", "package"]],
+	"scope-empty": [2, "never"]
 }
